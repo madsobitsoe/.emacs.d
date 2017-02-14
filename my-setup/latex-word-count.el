fb100d810459d5ebb4c184384ebf82ec1f81e6bf
Mads Obitsø <madsobitsoe@gmail.com>
2017-02-14 20:27:30 +0100

    latex-wc-mode.el

--- my-setup/latex-word-count.el
+++
@@ -1,9 +0,0 @@
-;; emacs wrapper to a script in python, ruby, etc.
-
-(defun latex-word-count (startPos endPos)
-  "Do some text processing on region.
-This command calls the external script “removelatexcode.pl, then pipes it to wc”."
-  (interactive "r")
-  (let (cmdStr)
-    (setq cmdStr "perl ~/.emacs.d/my-setup/removelatexcode.pl - | wc") ; full path to your script
-    (shell-command-on-region startPos endPos cmdStr nil nil nil t)))
